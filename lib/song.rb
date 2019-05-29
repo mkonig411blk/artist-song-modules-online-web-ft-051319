02bbb8c29d67858312eb4ef350cb5d07a8b8eca5
Margot Konig <margot.konig@blackrock.com>
2019-05-29 17:15:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
     extend Memorable::ClassMethods
     extend Findable::ClassMethods
     include Paramable
-    include extend Memorable::InstanceMethods
+    include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
