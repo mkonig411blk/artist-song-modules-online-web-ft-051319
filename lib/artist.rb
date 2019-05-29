c60431a99ca178128486db9eca2d9610babfe27c
Margot Konig <margot.konig@blackrock.com>
2019-05-29 17:09:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,6 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable
-  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
